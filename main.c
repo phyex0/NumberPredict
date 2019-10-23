6d13a185294cb6c89d6ccbcf72a2eed3fac248f8
phyex0 <48446413+phyex0@users.noreply.github.com>
2019-10-23 21:08:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+	srand(time(NULL));
+	int randNum=1+ rand() % 100;
+	printf("welcome to the number predictor\n");
+while(1){
+
+	while(1){
+		printf("give me number \n");
+		int x;
+		scanf("%d",&x);
+
+		if(x== randNum)	{
+			printf("You won\n");
+			break;
+		}
+		else if(x<randNum){
+			printf("too low ");
+		}
+
+        else if(x>randNum){
+			printf("too high ");
+		}
+
+    }
+
+	printf("To continue game press \'y\' Otherwise you're gonna quit \n");
+   char s;
+     scanf(" %c",&s);
+     if(s!='y'){
+	   break;
+               }
+
+
+}
+	return 0;
+}
